6b35426533794da036e7d7f96684324701ce4335
Haruki Koga <harukikoga.dev@gmail.com>
2023-09-03 01:08:11 +0900

    主にデザインの修正

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { getFirestore, collection, doc, getDocs, getDoc, query } from 'firebase/
 
 
 export default function Home() {
-  // DBをSTATEで管理
+  const [totalPoint, setTotalPoint] = useState(0);
   const [pointList, setPointList] = useState([]);
 
   // マウント時にDBと同期
@@ -21,8 +21,9 @@ export default function Home() {
     getPointList(firestoreDb)
   }, []);
 
-  async function getPointList(db:any) {
-    const allPoints:any = [];
+  // ポイントリスト取得
+  async function getPointList(db: any) {
+    const allPoints: any = [];
     // コレクション内のすべてのドキュメントを取得
     const querySnapshot = await getDocs(collection(db, "point"));
     querySnapshot.forEach((doc) => {
@@ -31,36 +32,35 @@ export default function Home() {
     setPointList(allPoints)
   }
 
-
-  function showPointList() {
-    console.log(pointList)
-  }
-
   return (
-    <main>
-
+    <>
       {/* タイトル */}
       <div className={styles.title}>
         <h1>Dekitane</h1>
-        <h2>~毎日のがんばりをポイントに~</h2>
+        <h2>~毎日のがんばりをポイント化~</h2>
       </div>
 
       {/* 今日溜まったポイント */}
 
-      {/* これまで溜まったポイント */}
-      <div>
-        <p>たまったポイント：{ }</p>
-      </div>
+      <main>
+        {/* これまで溜まったポイント */}
+        <div>
+          <p>たまったポイント：<span className={styles.totalPoint}>{totalPoint}</span></p>
+        </div>
 
-      {/* ポイント獲得表 */}
-      <div>
-        <ul>
-          {pointList.map((data:any, index) => (
-            <li key={index}>{data.name}</li>
-          ))}
-        </ul>
-      </div>
+        {/* ポイント獲得表 */}
+        <div className={styles.pointList}>
+          <ul>
+            {pointList.map((data: any, index) => (
+              <div className={styles.pointCol}>
+                <li key={index}>{data.name}<span className={styles.caption}>({data.point})</span></li>
+                <button className={styles.button}>達成!</button>
+              </div>
+            ))}
+          </ul>
+        </div>
 
-    </main>
+      </main>
+    </>
   );
 }
