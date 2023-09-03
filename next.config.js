25a448774c81276d5ebb9032caaa812d228c4d3c
Haruki Koga <harukikoga.dev@gmail.com>
2023-09-03 13:37:14 +0900

    ⚡️

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
+module.exports = {
+    env : {
+        // FIREBASE
+        FIREBASE_API_KEY: "AIzaSyCi2WUwD8_JBAwVdS000tLQb03b6oYydX0",
+        FIREBASE_AUTH_DOMAIN: "dekitane.firebaseapp.com",
+        FIREBASE_PROJECT_ID: "dekitane",
+        FIREBASE_STORAGE_BUCKET: "dekitane.appspot.com",
+        FIREBASE_MESSAGING_SENDER_ID: "75951588975",
+        FIREBASE_APP_ID: "1:75951588975:web:92284b7b7bdf6e984c2f9a",
+    },
+}
